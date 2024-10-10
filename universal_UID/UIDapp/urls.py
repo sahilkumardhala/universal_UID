edb6dc056c61850befa4365b421998a87c47ca3f
sahilkumardhala <sahildhala123@gmail.com>
2024-10-10 22:58:39 +0530

    2nd commit

--- universal_UID/UIDapp/urls.py
+++ universal_UID/UIDapp/urls.py
@@ -15,11 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from universal_UID.universal_UID import UIDapp
+from django.urls import path
 
 urlpatterns = [
-    # path('UID/', include("UIDapp.urls")),
-    path('', UIDapp.v),
     path('admin/', admin.site.urls),
 ]
