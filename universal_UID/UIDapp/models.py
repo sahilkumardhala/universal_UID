4a0fdaacb0c153ddb8ff3571b03aaaa4c11b07aa
Simran Mohapatra <simranmohapatra14@gmail.com>
2024-10-12 21:31:12 +0530

    UID_model,signup(),unique_UID create

--- universal_UID/UIDapp/models.py
+++ universal_UID/UIDapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from UIDapp.views import *
 # Create your models here.
+class UIDmodel(models.Model): 
+    username=models.CharField(max_length=60)
+    phno=models.IntegerField()
+    email=models.EmailField(max_length=100)
+
+    
+    UID = models.CharField(max_length=7, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.UID:  # If UID is not already set
+            self.UID = unique_user_UID()
+        super(UIDmodel, self).save(*args, **kwargs)
+
+
+    password=models.CharField(max_length=5,default="Welcome")
--- universal_UID/UIDapp/views.py
+++ universal_UID/UIDapp/views.py
@@ -1,17 +1,44 @@
 from django.shortcuts import render,HttpResponse
-
+import random
+from UIDapp.models import *
+# from django.db import models
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
-
-
 def loginpage(request):
     
     return render(request, 'login.html')
 
+def unique_user_UID():
+            prefix = "UID"
+            while True:
+                random_number = random.randint(1000, 9999)  # Generate a random number of 4 digits
+                UID = f"{prefix}{random_number}"
+                if not UIDmodel.objects.filter(UID=UID).exists():
+                    return UID
+
 def signup(request):
+    if request.method == "POST" :
+        username = request.POST.get("username")
+        phno=request.POST.get("phone")
+        email = request.POST.get("email")
+
+        
+        
+    
+        
+
+        
+
+
+
+
+
+
+
+
     return render(request, 'signup.html')   
 
 
